ef59f95f9efff5908e73792f38346700c019970a
jianyuanw <69199572+jianyuanw@users.noreply.github.com>
2021-04-07 22:55:11 +0800

    Added some handlers

--- bot.py
+++ bot.py
@@ -6,18 +6,18 @@ TOKEN = os.environ.get('TELEGRAM-BOT-TOKEN')
 import telegram
 bot = telegram.Bot(token=TOKEN)
 
-print(bot.get_me()) # verify if token is correct
+# Verify if token is correct
+print(bot.get_me())
 
 from telegram.ext import Updater
 updater = Updater(token=TOKEN)
-
 dispatcher = updater.dispatcher
 
+# Enable logging
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-### "/start" command handler ###
-
+# "/start" command handler
 def start(update, context):
   context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
@@ -25,8 +25,15 @@ from telegram.ext import CommandHandler
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
-### Message handler ###
+# "/caps" command handler
+def caps(update, context):
+    text_caps = ' '.join(context.args).upper()
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
 
+caps_handler = CommandHandler('caps', caps)
+dispatcher.add_handler(caps_handler)
+
+# Message handler that echos back the message
 def echo(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
@@ -34,4 +41,35 @@ from telegram.ext import MessageHandler, Filters
 echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
 dispatcher.add_handler(echo_handler)
 
+# Inline query handler
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+def inline_caps(update, context):
+    query = update.inline_query.query
+    if not query:
+        return
+    results = list()
+    results.append(
+        InlineQueryResultArticle(
+            id=query.upper(),
+            title='Caps',
+            input_message_content=InputTextMessageContent(query.upper())
+        )
+    )
+    context.bot.answer_inline_query(update.inline_query.id, results)
+
+from telegram.ext import InlineQueryHandler
+inline_caps_handler = InlineQueryHandler(inline_caps)
+dispatcher.add_handler(inline_caps_handler)
+
+# Default handler for unknown commands
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I don't understand that command.")
+
+unknown_handler = MessageHandler(Filters.command, unknown)
+dispatcher.add_handler(unknown_handler)
+
+# Start the bot
 updater.start_polling()
+
+# Run the bot until Ctrl-C is pressed
+updater.idle()
